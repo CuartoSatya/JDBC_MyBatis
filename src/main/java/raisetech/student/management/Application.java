af0c35e51c4986bcb06aa3e288ba49e5e5908382
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-03-16 14:25:30 +0900

    Merge remote-tracking branch 'origin/revise-student-info' into revise-student-info

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,37 +1,48 @@
 package raisetech.student.management;
 
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	private String name = "Oda Shintaro";
-	private String age = "44";
+	@Autowired
+	private StudentRepository repository;
 
-	private Map<String,String> student;
+	private String name = "Enami Kouji";
+	private int age = 44;
+
+	private Map<String, String> student;
 
 	public static void main(String[] args) {
 		//localhost:8080
 		SpringApplication.run(Application.class, args);
 	}
-
 	@GetMapping("/studentInfo")
-	public String getStudentInfo() {
-		return name + " " + age + " years old";
-				}
+	public String getstudentInfo() {
+		Student student = repository.findByName("Tanaka Taro");
+		if (student == null) {
+			return "Student not found";
+		}
+		return student.getName() + " " + student.getAge() + " years old";
+	}
+
 	@PostMapping("/studentInfo")
-	public void setStudentInfo(String name, String age) {
+	public void setName(@RequestParam String name, @RequestParam int age) {
 		this.name = name;
 		this.age = age;
 	}
+
 	@PostMapping("/studentName")
-	public void UpdateStudentName(String name) {
+	public void updateStudentName(@RequestParam String name){
 		this.name = name;
 	}
  }
