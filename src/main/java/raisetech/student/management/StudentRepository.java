c31d98276a5ac03d1e035e6c204cc7d9e9c9f74c
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-03-22 09:33:27 +0900

    Merge remote-tracking branch 'origin/revise-student-info' into revise-student-info

--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,5 +1,6 @@
 package raisetech.student.management;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface StudentRepository {
     @Select("SELECT * FROM student WHERE name = #{name}")
     Student findByName(String name);
+    @Insert("INSERT student values(#{name},#{age})")
+    void registerStudent(String name, int age);
 }
