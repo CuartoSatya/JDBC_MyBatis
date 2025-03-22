0eff539f4947e9fa4db09ca94b6e05f878a4fbee
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-03-22 10:25:44 +0900

    Merge remote-tracking branch 'origin/revise-student-info' into revise-student-info

--- src/main/java/raisetech/student/management/Student.java
+++ src/main/java/raisetech/student/management/Student.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 public class Student {
+    private int id;
     private String name;
     private int age;
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getName() {
         return name;
--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,6 +1,7 @@
 package raisetech.student.management;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,6 @@ public interface StudentRepository {
     @Select("SELECT * FROM student WHERE name = #{name}")
     Student findByName(String name);
     @Insert("INSERT INTO student (name, age) VALUES (#{name}, #{age})")
-    void registerStudent(String name, int age);
+    void registerStudent(@Param("name") String name, @Param("age") int age);
 
 }
