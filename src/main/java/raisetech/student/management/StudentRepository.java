db3b4f7470cf2f907c980ff58356dbae8e691f75
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-04-20 15:59:30 +0900

    Remove build artifacts from Git tracking

--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,12 +1,11 @@
 package raisetech.student.management;
 
 import org.apache.ibatis.annotations.*;
-
 import java.util.List;
 
 @Mapper
 public interface StudentRepository {
 
     @Select("SELECT * FROM student WHERE name LIKE CONCAT('%', #{name}, '%'))")
-    List<raisetech.student.management.Student> searchByName(@Param("name") String name);
+    List<raisetech.student.management.Student> findAll();
 }
