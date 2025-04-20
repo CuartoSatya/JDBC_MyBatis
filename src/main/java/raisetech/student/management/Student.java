96e82d03235986c95ff4e364fde9166be8b621af
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-04-20 16:35:46 +0900

    Remove build artifacts from Git tracking

--- src/main/java/raisetech/student/management/Student.java
+++ src/main/java/raisetech/student/management/Student.java
@@ -1,14 +1,18 @@
 package raisetech.student.management;
 
 public class Student {
-
-    private int id;
+    private int idStudent;
     private String name;
+    private String kanaName;
+    private String nickname;
+    private String mailAddress;
+    private String address;
     private int age;
+    private int sex;
 
-    public int getId() { return id; }
+    public int getId() { return idStudent; }
 
-    public void setId(int id) { this.id = id; }
+    public void setId(int id) { this.idStudent = id; }
 
     public String getName() {
         return name;
@@ -18,11 +22,45 @@ public class Student {
         this.name = name;
     }
 
+    public String getKanaName() {
+        return kanaName;
+    }
+
+    public void setKanaName(String KanaName) {
+        this.kanaName = name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = name;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) { this.mailAddress = name;}
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) { this.address = name;}
+
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setSex(int sex) {
         this.age = age;
+    }
+
+    public int getSex() { return sex; }
+
+    public void setAge(int sex) {
+        this.sex = age;
     }
 }
--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,6 +1,8 @@
 package raisetech.student.management;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
@@ -8,5 +10,12 @@ import java.util.List;
 public interface StudentRepository {
 
     @Select("SELECT * FROM student WHERE name LIKE CONCAT('%', #{name}, '%')")
+    @Select("SELECT * FROM student")
+    @Results({
+            @Result(property = "idStudent", column = "id_student"),
+            @Result(property = "kanaName", column = "kana_name"),
+            @Result(property = "mailAddress", column = "mail_address")
+            // 他のカラムも同様に追加
+    })
     List<Student> findAll();
 }
