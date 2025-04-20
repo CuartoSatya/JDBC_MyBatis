629f0672fba7feb388ee074c27f90ddfeac3e552
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-04-20 16:35:46 +0900

    Remove build artifacts from Git tracking

--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Mapper
 public interface StudentRepository {
 
-    @Select("SELECT * FROM student WHERE name LIKE CONCAT('%', #{name}, '%')")
+    @Select("SELECT id_student AS idstudent, name, kana_name AS kanaName, nickname, mail_address AS mailAddress, address, age, sex FROM student WHERE name LIKE CONCAT('%', #{name}, '%')")
     @Results({
             @Result(property = "idStudent", column = "id_student"),
             @Result(property = "kanaName", column = "kana_name"),
