493167cc543255b56d90ae33ed2dadba4d9bd008
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-04-23 22:23:14 +0900

    Remove build artifacts from Git tracking

--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,8 +1,6 @@
 package raisetech.student.management;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
@@ -10,15 +8,5 @@ import java.util.List;
 public interface StudentRepository {
 
     @Select("SELECT id_student AS id, name, kana_name AS kanaName, nickname, mail_address AS mailAddress, address, age, sex FROM student WHERE name LIKE CONCAT('%', #{name}, '%')")
-    @Results({
-            @Result(property = "id", column = "id_student"),
-            @Result(property = "kanaName", column = "kana_name"),
-            @Result(property = "mailAddress", column = "mail_address"),
-            @Result(property = "name", column = "name"),
-            @Result(property = "nickname", column = "nickname"),
-            @Result(property = "address", column = "address"),
-            @Result(property = "age", column = "age"),
-            @Result(property = "sex", column = "sex")
-    })
     List<Student> findByName(String name);
 }
