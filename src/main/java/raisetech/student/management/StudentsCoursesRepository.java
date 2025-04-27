d0d523438a11715038c2ae1d4a50e50052079947
Cuarto Satya <kmxs@mx52.tiki.ne.jp>
2025-04-27 11:25:51 +0900

    Remove build artifacts from Git tracking

---
+++ src/main/java/raisetech/student/management/StudentsCoursesRepository.java
@@ -0,0 +1,13 @@
+package raisetech.student.management;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+    @Mapper
+    public interface StudentsCoursesRepository {
+        @Select("SELECT id, student_id AS studentId, name, starting_date AS startingDate, assured_finishing_date " +
+                "AS assuredFinishingDate FROM students_courses")
+        List<StudentsCourses> searchAll();
+    }
